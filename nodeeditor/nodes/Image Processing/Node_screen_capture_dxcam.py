79f83cfd16186f567353da8baa3fd451673a5ec4
Tim Henning <timhenning1997@gmail.com>
2022-08-31 19:02:06 +0200

    Added a comment to the import statement of screen_capture_dxcam

--- nodeeditor/nodes/Image Processing/Node_screen_capture_dxcam.py
+++ nodeeditor/nodes/Image Processing/Node_screen_capture_dxcam.py
@@ -3,6 +3,7 @@ import os
 from collections import OrderedDict
 from pathlib import Path
 
+# pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple dxcam
 import dxcam
 import numpy as np
 
